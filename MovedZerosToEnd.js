76882cb4eeac77bcfe8a123472988f5a24a5752e
Shiva Kashyap <shiva.kashyap0008@gmail.com>
2025-11-06 00:11:32 +0530

    added move-zero-to-end question

---
+++ MovedZerosToEnd.js
@@ -0,0 +1,11 @@
+function moveZeros(arr) {
+  let j = 0; // pointer for non-zero position
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== 0) [arr[i], arr[j++]] = [arr[j], arr[i]];
+  }
+  return arr;
+}
+
+const arr = [0, 1, 0, 3, 12];
+console.log(moveZeros(arr));
+//merge sort, count frequency
---
+++ String/ReverseString.js
@@ -0,0 +1,10 @@
+function reverseString(str){
+
+    let res = ""
+    for(let i= str.length ; i>0; i--){
+          
+    }
+
+}
+
+const str = "hello"
